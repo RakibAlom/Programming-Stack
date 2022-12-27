3035489a46442c724bacede4333edda6abca130b
RakibAlom <rakibalom17@gmail.com>
2022-12-27 15:35:48 +0600

    react router dom and tailwind setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Programming-Stack</h1>
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/layouts/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
